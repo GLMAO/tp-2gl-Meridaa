729ecdbd2dd7332626fefbba7f1f32b37fdbb347
Souha <souha@example.com>
2025-11-20 14:05:54 +0100

    Exercice 2

--- src/main/java/com/polytech/tp/GestionnaireEmploiDuTemps.java
+++ src/main/java/com/polytech/tp/GestionnaireEmploiDuTemps.java
@@ -5,21 +5,41 @@ import java.util.List;
 
 public class GestionnaireEmploiDuTemps {
     private List<ICours> listeCours = new ArrayList<>();
+    private List<Observer> observateurs = new ArrayList<>();
+
+    // Ajouter un observateur
+    public void ajouterObservateur(Observer o) {
+        observateurs.add(o);
+    }
+    // Ajouter un observateur
+    public void supprimerObservateur(Observer o) {
+        observateurs.remove(o);
+    }
+    // Notifier tous les observateurs
+    private void notifierObservateurs(String message){
+        for(Observer o : observateurs){
+            o.update(message):
+        }
+    }
 
     public void ajouterCours(ICours cours) {
         this.listeCours.add(cours);
-        System.out.println("Nouveau cours ajouté : " + cours.getDescription());
         // TODO: C'est ici qu'il faudrait notifier les étudiants (Observer pattern)
+        String message = "Nouveau cours ajouté : " + cours.getDescription();
+        System.out.println(message);
+        notifierObservateurs(message);
     }
 
-    public void modifierCours(ICours cours, String message) {
+    public void modifierCours(ICours cours, String messageChangement ) {
         // Logique de modification...
-        System.out.println("Cours modifié : " + message);
         // TODO: Notifier les observateurs ici aussi
+        String message = "Cours modifié : " + cours.getDescription() + "=> " + messageChangement;
+        System.out.println(message);
+        notifierObservateurs(message);
     }
 
-    public void setChangement(String string) {
+    public void setChangement(String message) {
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setChangement'");
+        notifierObservateurs(message);
     }
 }
